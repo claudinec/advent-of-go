73e8747999df6782195bd5dc3bf407306e059620
Claudine Chionh (claudine) <git@claudinec.net>
2022-02-05 17:10:58 +1100

    WIP advent of code 2021 day 1

---
+++ advent2021/main.go
@@ -0,0 +1,21 @@
+package advent2021
+
+import (
+        "log"
+        "os"
+       )
+
+// Sonar Sweep: How many measurements are larger than the previous measurement?
+func Day01() {
+    data, err := os.ReadFile("input/1.txt")
+    if err != nil {
+		log.Fatal(err)
+	}
+    // do stuff
+}
+
+func Main() {
+    os.Chdir("advent2021")
+    Day01()
+}
+
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+        "claudinec/elves/advent2021"
+       )
+
+func main() {
+    advent2021.Main()
+}
+
